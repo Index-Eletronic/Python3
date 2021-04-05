6d94ad049953963ce7d3648b7da660c8026afc98
Fil Tuon <f.tuon887@gmail.com>
2021-04-05 16:28:08 -0300

    .

---
+++ exerc_lista_Expresao.py
@@ -0,0 +1,20 @@
+'''
+Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar
+ se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+'''
+
+expr = str(input('Digite uma expressão')) # Toda string é uma lista. E por usar o FOR para pegar cada letra/simbulo
+pilha = []
+for simb in expr: # Ira pegar cada simbolo.
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão esta válida')
+else:
+    print('Sua expressão está errada.')
