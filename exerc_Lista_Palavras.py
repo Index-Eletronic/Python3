b389b2ce44325edc06f7917bf886c07f45ebb218
Fil Tuon <f.tuon887@gmail.com>
2021-04-08 20:48:38 -0300

    .

--- exerc_Lista_Palavras.py
+++ exerc_Lista_Palavras.py
@@ -1,3 +1,16 @@
 '''
 Faça um programa que insira as palavras numa lista e corrijas.
 '''
+
+palavra = []
+
+while True:
+    palavra.append(str(input('Digite uma palabra:')))
+    resp = str(input('Deseja continuar? [S/N]: '))
+    if resp in "Nn":
+        break
+print(palavra)
+while True:
+    usuario = str(input('Digite a resposta:'))
+    for p, a in enumerate(palavra):
+        print(f'{p} em {a} tem {palavra}')
