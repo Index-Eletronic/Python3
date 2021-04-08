b44d27079cf0c954d20a9ae49787b568c9dfa7f1
Fil Tuon <f.tuon887@gmail.com>
2021-04-07 21:34:07 -0300

    .

--- exerc_lista2_Megasena.py
+++ exerc_lista2_Megasena.py
@@ -3,7 +3,7 @@ Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
 O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 '''
 from random import randint
-
+from time import sleep
 lista = list()
 jogos = list()
 print('-=' * 30)
@@ -25,6 +25,7 @@ while tot <= 0:
     lista.clear()
     tot += 1
 print('-=' * 3, f'SORTEANDO {quant} JOGOS','-=' * 3)
-print(jogos)
-#for i, l in enumerate(jogos):
-     #print(f'JOGO: {i+1}: {l:^3}')
+for i, l in enumerate(jogos):
+     print(f'JOGO: {i+1}: {l:^3}')
+     sleep(2)
+print('-=' * 3, 'BOA SORTE','-=' * 3)
