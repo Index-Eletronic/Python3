530248dcf1c0d14ba3b9f0874b209c9e2143bba6
Fil Tuon <f.tuon887@gmail.com>
2021-04-12 10:54:57 -0300

    .

---
+++ exerc_dict_teoria.py
@@ -0,0 +1,12 @@
+'''
+Teoria sobre Dicionários em python
+'''
+
+pessoa = {'nome': ' Filipe', 'sexo': 'M', 'idade': 25, }
+print(pessoa['nome'])
+print(pessoa['idade'])
+print(f'O{pessoa["nome"]} tem {pessoa["idade"]}') # Como esta dentro de ' ' (simples) tem que colocar "" ( duplas )
+print(pessoa.keys())
+print(pessoa.values())
+
+
