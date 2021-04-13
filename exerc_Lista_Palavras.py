75998a91e6a57f40f1f4a05cd64e3187534aa706
Fil Tuon <f.tuon887@gmail.com>
2021-04-13 11:17:18 -0300

    .

--- exerc_Lista_Palavras.py
+++ exerc_Lista_Palavras.py
@@ -1,16 +1,10 @@
 '''
-Faça um programa que insira as palavras numa lista e corrijas.
+Faça um programa que insira as palavras numa lista e diga se estão corretas..
 '''
+import random
 
-palavra = []
-
-while True:
-    palavra.append(str(input('Digite uma palabra:')))
-    resp = str(input('Deseja continuar? [S/N]: '))
-    if resp in "Nn":
-        break
-print(palavra)
-while True:
-    usuario = str(input('Digite a resposta:'))
-    for p, a in enumerate(palavra):
-        print(f'{p} em {a} tem {palavra}')
+select = random.choice(['acessível', 'agressivo', 'assim','assinar']).strip().upper()
+S = "SS"
+print(select)
+#for palavra in range(0, len(select)):
+ #       select = select.replace()
